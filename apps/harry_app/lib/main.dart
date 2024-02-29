153b4d536d725e7e738fc01321a2a35021c16a09
日野義斗 <hino.akito@nck.co.jp>
2024-02-29 21:32:23 +0900

    feat: Riverpodパッケージを設定し、MyWidgetを作成

--- apps/harry_app/lib/main.dart
+++ apps/harry_app/lib/main.dart
@@ -1,10 +1,31 @@
-// main.dart
+// ignore_for_file: unused_import
+
 import 'package:flutter/material.dart';
 
-import 'ui/home_app.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'my_widget.dart';
 
 void main() {
-  runApp(const HomeApp());
+  const app = MyApp();
+  const scope = ProviderScope(child: app); // Riverpodを使うためにProviderScopeでラップ
+  runApp(scope);
 }
 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: MyWidget(),
+          // child: MyWidget1(),
+          // child: MyWidget2(),
+          // child: MyWidget3(),
+          // child: MyWidget4(),
+        ),
+      ),
+    );
+  }
+}
